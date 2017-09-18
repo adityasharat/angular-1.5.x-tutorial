8a39efc034a5302b3d30ccc153ed8e85b1b38db0
Aditya Sharat <adityasharat@gmail.com>
2017-09-18 20:59:12 +0530

    Create the Todo Service

--- src/modules/todo/todo.js
+++ src/modules/todo/todo.js
@@ -7,6 +7,47 @@ class MainController {
   }
 }
 
+class Todo {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+  }
+}
+
+let TodoService = () => {
+
+  const todos = {};
+
+  function $create(title, description) {
+    todos[Object.keys().length] = new Todo(title, description);
+    return service;
+  }
+
+  function $add(todo) {
+    todos[Object.keys().length] = todo;
+    return service;
+  }
+
+  function $get(id) {
+    return id ? todos : todo.id;
+  }
+
+  function $update(todo) {
+    todos[todo.id] = todo;
+    return service;
+  }
+
+  function $delete(id) {
+    let todo = todos[id];
+    delete todos[id];
+    return todo;
+  }
+
+  const service = { $create, $add, $get, $update, $delete };
+
+  return service;
+};
+
 let todo = () => {
   return {
     template: require('./todo.html'),
@@ -19,6 +60,7 @@ const MODULE_NAME = 'todo';
 
 angular.module(MODULE_NAME, ['todo'])
   .directive('todo', todo)
-  .controller('MainController', MainController);
+  .controller('MainController', MainController)
+  .factory('TodoService', TodoService);
 
 export default MODULE_NAME;
