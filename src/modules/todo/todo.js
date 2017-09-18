569ad667f4bc6c723e01ed16d350ac152afd3572
Aditya Sharat <adityasharat@gmail.com>
2017-09-18 20:39:27 +0530

    remove mocking

--- src/modules/todo/todo.js
+++ src/modules/todo/todo.js
@@ -1,30 +1,9 @@
 import angular from 'angular';
 import '../../style/app.css';
 
-function randomString(length, limit) {
-  let str = [];
-  limit = typeof limit === 'number' ? limit : 100;
-  for (let i = 0; i < length; ++i) {
-    let random = Math.floor(Math.random() * limit);
-    str.push(String.fromCharCode(random));
-  }
-  return str.join('');
-}
-
-function mock(count) {
-  const todos = [];
-  while(count-- > 0) {
-    todos.push({
-      title: randomString(20),
-      description: randomString(80)
-    })
-  }
-  return todos;
-}
-
 class MainController {
   constructor() {
-    this.todos = mock(5);
+    this.todos = [];
   }
 }
 
